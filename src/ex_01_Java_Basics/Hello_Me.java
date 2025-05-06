8a4d3e3c47bc21dcc92e54395a7ddc3f55b841a0
ShraBhuj <srpingale04@gmail.com>
2025-05-06 22:59:12 +0530

    first commit

--- src/ex_01_Java_Basics/Hello_Me.java
+++ src/ex_01_Java_Basics/Hello_Me.java
@@ -1,4 +1,7 @@
 package ex_01_Java_Basics;
 
 public class Hello_Me {
+    public static void main(String[] args) {
+        System.out.println("Hello Shradha, welcome to ABT12x Java Class");
+    }
 }
