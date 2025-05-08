300232e72384364f311935d939e6a75a1dc89299
ShraBhuj <srpingale04@gmail.com>
2025-05-08 12:08:03 +0530

    first ever commit

---
+++ src/ex_01_Java_Basics/Java.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basics;
+
+public class Java {
+    public static void main(String[] args) {
+        System.out.println("Java is a set of computer software and specifications that provides a software platform for developing application software.");
+    }
+}
