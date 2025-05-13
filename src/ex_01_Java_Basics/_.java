bcedb3822d0575f99b5b3de75ea5ae73c5fe6f62
ShraBhuj <srpingale04@gmail.com>
2025-05-13 18:14:52 +0530

    second commit

--- src/ex_01_Java_Basics/_.java
+++ src/ex_01_Java_Basics/_.java
@@ -1,8 +1,8 @@
 package ex_01_Java_Basics;
 
-public class _ {
+//public class _ {
 
-    public static void main(String[] args) {
-        System.out.println("Test Class Name : _");
-    }
-}
+    //public static void main(String[] args) {
+//        System.out.println("Test Class Name : _");
+//    }
+//}
