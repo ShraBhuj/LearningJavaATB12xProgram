b49fc4ac96564973997965245f48c827e2975ea9
ShraBhuj <srpingale04@gmail.com>
2025-05-22 18:24:26 +0530

    all ever commit

---
+++ src/ex_02_Java_Basics_Part_2/Lab018_Variables_SpecialChar.java
@@ -0,0 +1,13 @@
+package ex_02_Java_Basics_Part_2;
+
+public class Lab018_Variables_SpecialChar {
+
+    public static void main(String[] args) {
+        int _ = 65;
+        int $ = 65;
+        int year = 2025;
+        int this_is_a_vari_long_name_in_the_class_hello_$_akakak = 89;
+
+        System.out.println(year);
+    }
+}
---
+++ src/ex_02_Java_Basics_Part_2/Lab019_Interview_QnA.java
@@ -0,0 +1,19 @@
+package ex_02_Java_Basics_Part_2;
+
+public class Lab019_Interview_QnA {
+
+    public static void main (String[] args)
+
+    {
+        int _2 = 33;
+        System.out.println(_2);
+
+        int _1 = 74;
+        System.out.println(_1);
+
+        int _0 = 87;
+        System.out.println(_0);
+
+
+    }
+}
